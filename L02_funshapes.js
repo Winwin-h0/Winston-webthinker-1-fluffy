658a78b04cce90bd217ca929dd342ea2f9f159e4
Winwin-h0 <winstonckho@gmail.com>
2026-07-08 08:02:39 +0000

    Jul 8, 2026, 8:02 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -35,7 +35,7 @@ function draw() {
   // ellipse(370,290,10,20);
 
   // Challenge 4: A rocketship using triangle() function
-  ellipse(150,90,200,100)
+  ellipse(150,90,1,100)
   triangle(100,90,150,0,200,90)
 
 
