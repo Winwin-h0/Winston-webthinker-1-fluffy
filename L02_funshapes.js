9e27b16cc7d992712a9a2e4a7c115dbc5708c9cf
Winwin-h0 <winstonckho@gmail.com>
2026-07-08 08:15:55 +0000

    Jul 8, 2026, 8:15 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -43,7 +43,8 @@ function draw() {
   // Challenge 5: A square face using quad() function
   quad(20,20,80,20,80,80,20,80);
   quad(120,30,180,30,180,70,120,70);
-  quad(50,162,86,150,50,138)
+  quad(50,162,86,150,50,138,14,150);
+  quad()
   // Challenge 6: Add some colours to the drawings in the above challenges
 
   // Challenge 7: Create a funny Pokemon
