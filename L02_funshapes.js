fa9ea64b1b5ae62bcb85b6b1810377cdfa4c3995
Winwin-h0 <winstonckho@gmail.com>
2026-07-08 08:12:26 +0000

    Jul 8, 2026, 8:12 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -35,13 +35,13 @@ function draw() {
   // ellipse(370,290,10,20);
 
   // Challenge 4: A rocketship using triangle() function
-  ellipse(145,150,110,200)
-  triangle(100,90,150,0,190,90)
-  rect(90,240,100,30)
-  circle(145,150,40)
+  // ellipse(145,150,110,200)
+  // triangle(100,90,150,0,190,90)
+  // rect(90,240,100,30)
+  // circle(145,150,40)
 
   // Challenge 5: A square face using quad() function
-  
+
   // Challenge 6: Add some colours to the drawings in the above challenges
 
   // Challenge 7: Create a funny Pokemon
