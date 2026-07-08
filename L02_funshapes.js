2e27acedf5d3ccbc1e81e89c36b163040ca718e9
Winwin-h0 <winstonckho@gmail.com>
2026-07-08 07:38:29 +0000

    Jul 8, 2026, 7:38 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -28,7 +28,7 @@ function draw() {
   // circle(160,170,50)
 
   // Challenge 3: A house using rect() function
-
+  rect()
   // Challenge 4: A rocketship using triangle() function
 
   // Challenge 5: A square face using quad() function
