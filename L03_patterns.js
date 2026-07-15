b4d924a49844aa79a2f913be32f518114b89b85e
Winwin-h0 <winstonckho@gmail.com>
2026-07-15 08:52:12 +0000

    Jul 15, 2026, 8:52 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -45,7 +45,7 @@ function draw() {
 
   for (let i = 0; i < numCircles; i++){
     // let x = startX + i * circleDiameter;
-    ellipse(15, 15, circleDiameter, circleDiameter);
+    ellipse(circleDiameter, 15, circleDiameter, circleDiameter);
   }
   // Task 4: Grid of Circles
 }
