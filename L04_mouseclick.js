65820ce22004101fabd3dbc313a951e6f0ec8bda
Winwin-h0 <winstonckho@gmail.com>
2026-07-26 03:42:45 +0000

    Jul 26, 2026, 3:42 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -15,5 +15,5 @@ function draw() {
         ypos = ypos + 50;//change of positon Y
         ggColor = ggColor + 50;//change of color
     }// to get the reverse, have a big X number and use -
-    
+    //
 }
