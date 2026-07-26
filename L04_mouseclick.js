c523941b606817abfec48fe166b80bacd405d506
Winwin-h0 <winstonckho@gmail.com>
2026-07-26 03:51:36 +0000

    Jul 26, 2026, 3:51 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -5,15 +5,17 @@ function setup() {
 }
 
 function draw() {
-    let xpos = 350; //x positon
-    let ypos = 50; //y positon
-    let ggColor= 0; //green color
-    for (let num = 1; num < 4; num++) {//repeat 5 times
-        fill(0, ggColor,0);//green color
-        circle(xpos,ypos,50);//positon of circle
-        xpos = xpos - 50;//change of positon X
-        ypos = ypos + 50;//change of positon Y
-        ggColor = ggColor + 50;//change of color
-    }// to get the reverse, have a big X number and use -
-    // the opposite  have a smaller number and use +
+    // let xpos = 350; //x positon
+    // let ypos = 50; //y positon
+    // let ggColor= 0; //green color
+    // for (let num = 1; num < 4; num++) {//repeat 5 times
+    //     fill(0, ggColor,0);//green color
+    //     circle(xpos,ypos,50);//positon of circle
+    //     xpos = xpos - 50;//change of positon X
+    //     ypos = ypos + 50;//change of positon Y
+    //     ggColor = ggColor + 50;//change of color
+    // }// to get the reverse, have a big X number and use -
+    // // the opposite  have a smaller number and use +
+
+    
 }
