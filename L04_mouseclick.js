b786677f625e60245cedd49d7144eb66552fab6e
Winwin-h0 <winstonckho@gmail.com>
2026-07-26 03:28:02 +0000

    Jul 26, 2026, 3:28 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -6,7 +6,7 @@ function setup() {
 function draw() {
     let xpos = 50;
     for (let num = 0; num < 5; num++) {
-        fill(num * 50)
+        fill(0);
         circle(50 + num * 50, 100, 40)
     }
 }
