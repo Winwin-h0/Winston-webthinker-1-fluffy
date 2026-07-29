ce237f241d01bfbc97f932983ce66f0efd5d8185
Winwin-h0 <winstonckho@gmail.com>
2026-07-29 07:56:22 +0000

    Jul 29, 2026, 7:56 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -8,7 +8,7 @@ function setup(){
 // function draw(){
 // }
 function keyPressed(){
-    circle(random(1,30))
+    circle(random(1,20),)
     // if (key==="w"){
     //      rectSize = 100;
     // }
