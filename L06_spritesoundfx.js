224820dc13e893da66113f65746fa2f5446c124e
Winwin-h0 <winstonckho@gmail.com>
2026-08-05 08:38:24 +0000

    Aug 5, 2026, 8:38 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -26,29 +26,29 @@ function keyPressed(){
 }
 
 function draw(){
-//     background('green')
+    background('green')
 
-//     square(x, y, 100)
+    square(x, y, 100)
 
-//     if (keyIsDown(DOWN_ARROW)){
-//         y = y + 50
-//     }
-//     if (keyIsDown(UP_ARROW)){
-//         y = y - 50
-//     }
-//     if (keyIsDown(LEFT_ARROW)){
-//         x = x - 50
-//     }
-//     if (keyIsDown(RIGHT_ARROW)){
-//         x = x + 50
-//     }
-//     if (keyIsDown(32)){
-//         x = x + 50
-//         fill(random(50,200))
-//     }
+    if (keyIsDown(DOWN_ARROW)){
+        y = y + 50
+    }
+    if (keyIsDown(UP_ARROW)){
+        y = y - 50
+    }
+        if (keyIsDown(LEFT_ARROW)){
+            x = x - 50
+    }
+    if (keyIsDown(RIGHT_ARROW)){
+        x = x + 50
+    }
+    if (keyIsDown(32)){
+        x = x + 50
+        fill(random(50,200))
+    }
 
-//     x = constrain(x,300,300)
-//     y = constrain(y,300,300)
+    x = constrain(x,300,300)
+    y = constrain(y,300,300)
 }
 
 
