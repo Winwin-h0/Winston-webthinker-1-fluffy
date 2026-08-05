ee7effa2d6064cdd614e36c6e8cfafe9ef06e63e
Winwin-h0 <winstonckho@gmail.com>
2026-08-05 08:27:56 +0000

    Aug 5, 2026, 8:27 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -5,7 +5,7 @@ let soundEffect
 
 function preload(){
     staticImage = loadImage('assets/pico-a.png');
-    soundEffect = loadSound('assests/pop.mp3');
+    soundEffect = loadSound('assets/pop.mp3');
 }
 
 function setup(){
