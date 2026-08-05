1a628c938cea973c60108406b40641082209580c
Winwin-h0 <winstonckho@gmail.com>
2026-08-05 08:48:12 +0000

    Aug 5, 2026, 8:48 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -39,7 +39,7 @@ function draw(){
     }
     if (keyIsDown(32)){
     }
-    staticImageX = contrain(staticImageX,0,300)
+    staticImageX = contrain(staticImageX,0,250)
     staticImageY = contrain(staticImageY,300,300)
 }
 
