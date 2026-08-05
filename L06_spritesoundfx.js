f19d19cfbfff7d1e76a2aa2551012980510a8f48
Winwin-h0 <winstonckho@gmail.com>
2026-08-05 08:39:02 +0000

    Aug 5, 2026, 8:39 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -28,18 +28,14 @@ function keyPressed(){
 function draw(){
     background('green')
 
-    square(x, y, 100)
-
     if (keyIsDown(DOWN_ARROW)){
     }
     if (keyIsDown(UP_ARROW)){
 
     }
     if (keyIsDown(LEFT_ARROW)){
-
     }
     if (keyIsDown(RIGHT_ARROW)){
-
     }
     if (keyIsDown(32)){
 
