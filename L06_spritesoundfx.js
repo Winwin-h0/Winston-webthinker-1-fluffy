771ca346b14a27c96f1e5e171991102eed277c83
Winwin-h0 <winstonckho@gmail.com>
2026-08-05 08:34:53 +0000

    Aug 5, 2026, 8:34 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -11,7 +11,7 @@ function preload(){
 function setup(){
     createCanvas(400,400);
     background('green');
-    // image(staticImage,0,0,100,100);
+    image(staticImage,0,0,100,100);
 }
 
 function keyPressed(){
