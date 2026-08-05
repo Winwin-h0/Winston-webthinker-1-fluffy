050234445f190c7bbf9a30107930514d673e21a0
Winwin-h0 <winstonckho@gmail.com>
2026-08-05 07:52:41 +0000

    Aug 5, 2026, 7:52 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,1 +1,25 @@
-// write your codes here
+x = 400 / 2
+y = 400 / 2
+function setup(){
+    createCanvas(400,400)
+    background('green')
+}
+
+function draw(){
+    square(x, y, 100)
+
+}
+
+function keyPressed(){
+    if(key === "down_arrow")
+        y = y - 50
+    if(key === "up_arrow")
+        y = y + 50
+    if(key === "left_arrow")
+        x = x - 50
+    if(key === "right_arrow")
+        x = x + 50
+
+    if(key === "space_bar")
+        fill(random(50,200))
+}
