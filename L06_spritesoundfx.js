38fc33f60751dc6b56de17b7fe4d607dded3dcab
Winwin-h0 <winstonckho@gmail.com>
2026-08-05 08:01:26 +0000

    Aug 5, 2026, 8:01 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -17,9 +17,12 @@ function keyIsDown(){
     if(keyCode === UP_ARROW){
         y = y - 50
     }
-    if(keyCode === LEFT_ARROW)
+    if(keyCode === LEFT_ARROW){
         x = x - 50
-    if(keyCode === RIGHT_ARROW)
+    }
+    if(keyCode === RIGHT_ARROW){
+        
+    }
         x = x + 50
     if(keyCode === 32)
         fill(random(50,200))
