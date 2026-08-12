d12f164953d201ce17af35bf6542b7d21080ea3c
Winwin-h0 <winstonckho@gmail.com>
2026-08-12 07:46:18 +0000

    Aug 12, 2026, 7:46 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -46,7 +46,7 @@ function draw(){
     if (keyIsDown(32)){
         newImageWidth = imageWidth/2
     }else{
-        newImageWidth = imageWidth
+        newImageWidth = imageWidth  
     }
     staticImageX = constrain(staticImageX,0,400 - 50)
 }
