dfb3d3a739c1e65337e4dc242dd90d4990a0d264
Winwin-h0 <winstonckho@gmail.com>
2026-08-12 07:47:44 +0000

    Aug 12, 2026, 7:47 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -37,7 +37,7 @@ function draw(){
         staticImageY = staticImageY + staticSpeed
     }
     if (keyIsDown(UP_ARROW)){
-        
+        staticImage = staticImageY + staticSpeed
     }
     if (keyIsDown(LEFT_ARROW)){
         staticImageX = staticImageX - staticSpeed
