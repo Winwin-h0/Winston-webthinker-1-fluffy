90f30a0872da4582ab9c52a5677a05e7b8c0f35f
Winwin-h0 <winstonckho@gmail.com>
2026-08-12 07:57:24 +0000

    Aug 12, 2026, 7:57 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -5,11 +5,11 @@ let ballSpeedX = 2
 let ballSpeedY = 2
 function setup(){
     createCanvas(600,400)
-    background('green')
+    
 }
 
 function draw(){
-    // background('green')
+    background('green')
 
     ballX = ballX + ballSpeedX
     ballY = ballY + ballSpeedY
