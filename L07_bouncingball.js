eb25cb72cbbbf9b22c909f9ea3f17c8229021b7a
Winwin-h0 <winstonckho@gmail.com>
2026-08-12 08:52:44 +0000

    Aug 12, 2026, 8:52 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -29,7 +29,7 @@ function draw(){
 
     if (ballX - ballSize/2 <= 0 || ballX + ballSize/2 >= width){
         ballSpeedX = ballSpeedX * -1;
-        
+        popSound.play
         if(toggleColour === true){
             fill(random(255),random(255),random(255))
         }
