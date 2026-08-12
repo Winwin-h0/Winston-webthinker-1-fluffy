06d3f8aff15eff54e75bb83c29817d13fa3b9eaf
Winwin-h0 <winstonckho@gmail.com>
2026-08-12 08:11:10 +0000

    Aug 12, 2026, 8:11 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -15,14 +15,14 @@ function draw(){
     ballY = ballY + ballSpeedY
 
     if (ballX - ballSize/2 <= 0 || ballX + ballSize/2 >= width){
-        ballSpeedX = ballSpeedX * -1
+        ballSpeedX = ballSpeedX * -1;
     }
 
     if (ballY - ballSize/2 <= 0 || ballY + ballSize/2 >= height){
-        ballSpeedY = ballSpeedY * -1
+        ballSpeedY = ballSpeedY * -1;
     }
 
-    noStroke()
-    fill(255)
-    circle(ballX,ballY,ballSize)
+    noStroke();
+    fill(255);
+    circle(ballX,ballY,ballSize);
 }
