c794024b1f3a5930ec2a99a8df913c99750db103
Winwin-h0 <winstonckho@gmail.com>
2026-08-19 08:23:33 +0000

    Aug 19, 2026, 8:23 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -11,12 +11,8 @@ function draw(){
     background('green')
 
     fill(0);
-    // text("My Favourite Foods:",50,50)
     text("My favourite Hobbys:",50,50)
 
-    // for(let i = 0; i < favouriteFoods.length; i++){
-    //     text((i + 1) + "." + favouriteFoods[i], 50, 80 + i * 30)
-    // }
     for(let i = 0; i < favouriteHobbys.lenght; i++){
         text((i + 1) + "." + favouriteHobbys[i], 50, 80 + i * 30)
     }
