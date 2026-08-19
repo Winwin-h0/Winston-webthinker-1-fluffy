cac0743b9ab2349059a7f8b747749a052804cdc1
Winwin-h0 <winstonckho@gmail.com>
2026-08-19 08:40:02 +0000

    Aug 19, 2026, 8:40 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -30,7 +30,7 @@ function draw(){
 
     for (let i = 0; i < storyText.lenght; i++){
         let sentence = storyText[i]
-        text(sentence,width/2, yPos +i * lineGap)
+        text(sentence,width/2, yPos + i * lineGap)
     }
     // yPos -= 0.6
 
