3959cf44af6fd21a5855e263df5adbad9ec6f1cb
Winwin-h0 <winstonckho@gmail.com>
2026-08-19 08:04:47 +0000

    Aug 19, 2026, 8:04 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -7,12 +7,7 @@ function setup(){
     createCanvas(600,400);
     background('green')
 
-    textSize(16)
-    text("My name is Winston",50,50)
-
-    text("I am 10 year old this year.",50,100)
-
-    text("My favourite activity is playing video games.",50,150)
+    textSize(24)
 }
 
 function draw(){
