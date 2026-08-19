22f06b110bc71e39ffd9102962804f82ce4c4d40
Winwin-h0 <winstonckho@gmail.com>
2026-08-19 07:44:32 +0000

    Aug 19, 2026, 7:44 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -2,6 +2,7 @@ let squareX = 600/2
 let squareY = 400/2
 let squareSize = 50
 
+
 function setup(){
     createCanvas(600,400)
 }
@@ -11,7 +12,7 @@ function draw(){
     squareX += 10
     constrain(squareX,600,0)
     if (squareX - squareSize/2 <= 0 || squareX + squareSize/2 >= width){
-        squareX = squX * -1;
+        squareX = X * -1;
     }
     fill(random(255),random(255),random(255))
     square(squareX,squareY,ballSize)
