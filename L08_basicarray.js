9e627520aade3b3e82c38b21a811f0b6b58527e4
Winwin-h0 <winstonckho@gmail.com>
2026-08-19 08:18:25 +0000

    Aug 19, 2026, 8:18 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,5 +1,5 @@
 let favouriteFoods = ["Pizza","Sashimi","Sushi","Tacos","pasta"]
-let favouriteGames = ["","minecraft"]
+let favouriteHobbys = ["","","",]
 
 function setup(){
     createCanvas(600,400);
