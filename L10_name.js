06b67a96c39e28bb53c65cf79ac92da90ce00f29
Winwin-h0 <winstonckho@gmail.com>
2026-09-02 08:32:16 +0000

    Sep 2, 2026, 8:32 AM

--- L10_name.js
+++ L10_name.js
@@ -2,12 +2,13 @@ let inputText;
 let userText = "Your Text Here";
 function setup(){
     createCanvas(600,400)
+
+    inputText = createInput()
+    inputText.position(200,height -80)
+    inputText.input(updateText)
 }
 
 function draw(){
     background('green')
 
-    inputText = createInput()
-    inputText.position(200,height -80)
-    inputText.input(updateText)
 }
