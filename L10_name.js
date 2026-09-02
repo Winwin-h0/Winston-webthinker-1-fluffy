14a574f2dba3649f6e8c67c6edc44c3fc51f6428
Winwin-h0 <winstonckho@gmail.com>
2026-09-02 08:45:02 +0000

    Sep 2, 2026, 8:45 AM

--- L10_name.js
+++ L10_name.js
@@ -5,7 +5,7 @@ function setup(){
     createCanvas(600, 400);
 
     inputText = createInput();
-    inputText.position(200,height -80);
+    inputText.position(200, height -80);
     inputText.input(updateText);
 }
 
