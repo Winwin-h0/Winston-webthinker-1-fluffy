309e457e0e0bec913edc838890b21158772d130e
Winwin-h0 <winstonckho@gmail.com>
2026-09-09 08:06:05 +0000

    Sep 9, 2026, 8:06 AM

--- L10_name.js
+++ L10_name.js
@@ -31,7 +31,7 @@ function draw(){
     textAlign(CENTER,CENTER)
     text(userText1, width/2, 100);
     text(userText2, width/2, 150)
-    text("")
+    text("Pick a background color")
 }
 
 function updateText(){
