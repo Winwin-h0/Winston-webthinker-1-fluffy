d21c0bdea26d4fa4d38a0040991232c9cd7d7ad9
Winwin-h0 <winstonckho@gmail.com>
2026-09-09 08:30:19 +0000

    Sep 9, 2026, 8:30 AM

--- L10_name.js
+++ L10_name.js
@@ -39,7 +39,7 @@ function draw(){
     textSize(16);
     fill(0);
     text("Enter your name")
-    text("Pick a background color:", 20, 560)
+    text("Pick a background color:", 50, 360)
 }
 
 function updateText(){
