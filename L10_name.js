2f9ea6f4955ff23e1707730a09f7f11f493fca84
Winwin-h0 <winstonckho@gmail.com>
2026-09-09 08:16:44 +0000

    Sep 9, 2026, 8:16 AM

--- L10_name.js
+++ L10_name.js
@@ -31,7 +31,7 @@ function draw(){
 
     fill(0);
     textSize(28);
-    textAlign(BOTTEM,BOTTEM)
+    textAlign(BOTTEM,CENTER)
     text(userText1, 300, 400);
     text(userText2, 300, 450)
     text("Pick a background color;", 20, 50)
