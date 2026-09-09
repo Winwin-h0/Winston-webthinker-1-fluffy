3146c0e4dcbaafe5b47760fb4630e05b812d21ce
Winwin-h0 <winstonckho@gmail.com>
2026-09-09 08:41:09 +0000

    Sep 9, 2026, 8:41 AM

--- L10_name.js
+++ L10_name.js
@@ -28,7 +28,7 @@ function setup(){
     colorPickerForRect = createColorPicker("rgb(255, 254, 254)");
     colorPickerForRect. position(500,500);
 
-    colorPickerForText
+    colorPickerForText = createColorP
 
 
 }
