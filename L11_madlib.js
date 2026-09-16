79af7fef161e820ec1a637c924fd8f857d29a9ec
Winwin-h0 <winstonckho@gmail.com>
2026-09-16 08:13:11 +0000

    Sep 16, 2026, 8:13 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -9,6 +9,18 @@ function setup(){
     inputFieldName = createInput("Enter your text here")
     inputFieldName.position(55,20)
 
+    inputFieldName = createInput("Enter your text here")
+    inputFieldName.position(55,20)
+
+    inputFieldName = createInput("Enter your text here")
+    inputFieldName.position(55,20)
+
+    inputFieldName = createInput("Enter your text here")
+    inputFieldName.position(55,20)
+
+    inputFieldName = createInput("Enter your text here")
+    inputFieldName.position(55,20)
+
     // submitButtonForName = createButton("Show Input")
     // submitButtonForName.mousePressed(displayInput);
     // submitButtonForName.position(inputFieldName.x + inputFieldName.width + 10, inputFieldName.y)
