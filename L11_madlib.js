2d8ec56448d1e041b22fe130eb2e4965c20d2773
Winwin-h0 <winstonckho@gmail.com>
2026-09-16 08:24:42 +0000

    Sep 16, 2026, 8:24 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -28,7 +28,7 @@ function setup(){
     fill(0)
     textSize(15)
     text("Enter a noun:", 100, inputFieldName.y)
-    text("Enter a verb:"")
+    text("Enter a verb:", 100, input)
     
 
     // submitButtonForName = createButton("Show Input")
