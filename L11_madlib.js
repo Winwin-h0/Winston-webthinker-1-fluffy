4706eabdfb516c8ecfd07aa07afcbdacf28064a2
Winwin-h0 <winstonckho@gmail.com>
2026-09-16 08:00:00 +0000

    Sep 16, 2026, 8:00 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -9,5 +9,5 @@ function setup(){
     inputField = createInput("Enter your text here")
     inputField.position(55,20)
 
-    submitButton = createButton("")
+    submitButton = createButton("Show Input")
 }
