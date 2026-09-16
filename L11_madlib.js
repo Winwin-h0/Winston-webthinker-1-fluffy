4520d5bba07874a289562fb7d2d1a7894e300a23
Winwin-h0 <winstonckho@gmail.com>
2026-09-16 08:01:26 +0000

    Sep 16, 2026, 8:01 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -11,5 +11,5 @@ function setup(){
 
     submitButton = createButton("Show Input")
     submitButton.mousePressed(displayInput);
-    submitButton.position(inputField.x + input)
+    submitButton.position(inputField.x + inputField.width)
 }
