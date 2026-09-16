06a93e51ab49e7ac119fd37916a0be7959a96c7d
Winwin-h0 <winstonckho@gmail.com>
2026-09-16 07:35:13 +0000

    Sep 16, 2026, 7:35 AM

--- L11_recap.js
+++ L11_recap.js
@@ -1,6 +1,10 @@
 function setup(){
     createCanvas(200,200)
     background('green')
+
+    inputText = createInput();
+    inputText.position(200, 500);
+    inputText.input(updateText);
 }
 
 function draw(){
