cdbc5d5bd70d65448fa444407d24ce4950521141
Winwin-h0 <winstonckho@gmail.com>
2026-09-16 08:44:23 +0000

    Sep 16, 2026, 8:44 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -37,7 +37,7 @@ function setup(){
     text("Enter a place:", 100, inputFieldPlace.y + 15);
 
     storyTemplates = [
-        "The {"
+        "The {adj} {"
     ]
 
     submitButtonForName = createButton("Show Input")
