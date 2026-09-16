1f299b836ae672e3639740b9238796566d6c578b
Winwin-h0 <winstonckho@gmail.com>
2026-09-16 08:49:11 +0000

    Sep 16, 2026, 8:49 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -45,7 +45,7 @@ function setup(){
     let template = random(storyTemplates);
 
     storyText = template.replace("{noun}", "dog")
-                        .replace("{verb}", )
+                        .replace("{verb}", "jump")
 
     submitButtonForName = createButton("Show Input")
     submitButtonForName.mousePressed(displayInput);
