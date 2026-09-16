f7be88949c7387d3be242b8904195790541d5bdc
Winwin-h0 <winstonckho@gmail.com>
2026-09-16 07:40:31 +0000

    Sep 16, 2026, 7:40 AM

--- L11_recap.js
+++ L11_recap.js
@@ -4,7 +4,7 @@ function setup(){
     background('green')
 
     inputText = createInput();
-    inputText.position(10, 300);
+    inputText.position(10, 100);
     inputText.input(updateText);
 
 }
