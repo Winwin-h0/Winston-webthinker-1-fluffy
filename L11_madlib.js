6134cd703f041dce1dc97cb1fd87d009f455f77a
Winwin-h0 <winstonckho@gmail.com>
2026-09-16 08:45:41 +0000

    Sep 16, 2026, 8:45 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -38,7 +38,7 @@ function setup(){
 
     storyTemplates = [
         "The {adj} {noun} decided to {verb} {adj} at the {place}.",
-        "One day, a {adj} {noun}"
+        "One day, a {adj} {noun} wanted to {verb}"
     ]
 
     submitButtonForName = createButton("Show Input")
