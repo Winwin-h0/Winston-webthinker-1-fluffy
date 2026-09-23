a4b8b3fa68386aea8f6940e98a471c59e38645ac
Winwin-h0 <winstonckho@gmail.com>
2026-09-23 08:02:20 +0000

    Sep 23, 2026, 8:02 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -8,7 +8,7 @@ function setup(){
 
     inputFieldGuess = createInput("Enter your text here");
     inputFieldGuess.style(150,30)
-    inputFieldGuess.style("font")
+    inputFieldGuess.style("font-size","20px")
     inputFieldGuess.position(200,20);
 
     buttonForGuess = createButton("Show Input");
