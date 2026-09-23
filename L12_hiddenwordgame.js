7945a814462d21ac37dd0af1e26bbb0723bb1a07
Winwin-h0 <winstonckho@gmail.com>
2026-09-23 08:04:38 +0000

    Sep 23, 2026, 8:04 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -3,17 +3,17 @@ let inputFieldGuess;
 let buttonForGuess;
 
 function setup(){
-    createCanvas(600,400)
-    background('green')
+    createCanvas(600,400);
+    background('green');
 
     inputFieldGuess = createInput("Enter your text here");
-    inputFieldGuess.size(150,30)
-    inputFieldGuess.style("font-size","20px")
+    inputFieldGuess.size(150,30);
+    inputFieldGuess.style("font-size","20px");
     inputFieldGuess.position(200,20);
 
     buttonForGuess = createButton("Show Input");
     buttonForGuess.mousePressed(displayInput);
-    buttonForGuess.size(150,30)
+    buttonForGuess.size(150,30);
     buttonForGuess.style("font-size", "20px");
     buttonForGuess.position(inputFieldGuess.x + inputFieldGuess.width + 10, inputFieldGuess.y);
 
