55a601db0642cfea227ff40b118ccecbc9dfe11b
Winwin-h0 <winstonckho@gmail.com>
2026-09-23 08:34:19 +0000

    Sep 23, 2026, 8:34 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -43,7 +43,7 @@ function draw(){
     text("Attempts: 0", 300, 100)
     text("Hint: " + hiddenWord, 300, 150)
 
-    text("Hint:")
+    text("Hint:" + display)
 
     //text(displayWord, 300, 300)
 }
