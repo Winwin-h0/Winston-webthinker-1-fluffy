29ddc956215472a7d10f753556cf7b8f4246af38
Winwin-h0 <winstonckho@gmail.com>
2026-09-23 07:36:56 +0000

    Sep 23, 2026, 7:36 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -51,6 +51,15 @@ function setup(){
 function displayInput(){
     background(220);
 
+    fill(0);
+    textSize(15);
+    text("Enter a noun:", 100, inputFieldName.y + 15);
+    text("Enter a verb:", 100, inputFieldVerb.y + 15);
+    text("Enter a adjective:", 70, inputFieldAdjective.y + 15);
+    text("Enter a adverb:", 80, inputFieldAdverb.y + 15);
+    text("Enter a place:", 100, inputFieldPlace.y + 15);
+
+
     let template = random(storyTemplates);
 
     storyText = template.replace("{noun}", inputFieldName.value())
