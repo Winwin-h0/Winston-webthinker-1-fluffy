b5f02743ca81a09afed36de29b1943e988d294f8
Winwin-h0 <winstonckho@gmail.com>
2026-09-23 08:51:27 +0000

    Sep 23, 2026, 8:51 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -58,6 +58,8 @@ function displayInput(){
 function getCorrectLetters(guess,word){
     let correctLetters = "";
     for(let i = 0; i < word.length; i++){
-        
+        if(word.includes(guess[i]) && !correctLetters.includes(guess[i].toUpperCase())){
+            
+        }
     }
 }
